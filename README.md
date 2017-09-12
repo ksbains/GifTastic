2510d3df8bf7a93cd3b2281be1a064eeb74247f5
ksbains <k1bains@ucsd.edu>
2017-09-11 18:10:26 -0700

    got the stills to show up but still nedd to make them go and stop

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GifTastic
 Homework 6
+
+boottrap code
+<!--  <div class="container">
+      <div class = "row">
+        <div class= "col-sm-12" id= "buttonSpace">
+          <div id="buttons-view"></div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-9" id="gifDisplay">
+          <div id="gif-view"></div>
+        </div>
+         <div class="col-sm-3" id="gifSearch">
+          <h1>Gif Search</h1>
+          <form id="gif-form">
+            <label for="gif-input">Add a Animal</label>
+            <input type="text" id="gif-input"><br>
+            <input id="add-gif" type="submit" value="Add a Gif">
+          </form>
+         </div>
+      </div>
+    </div>
+ -->
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,23 @@
-  // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  // <script type="text/javascript">
-// Initial array of movies
+function switcharoo(boolean){
+  if(boolean){
+    //
+
+  }
+}
 var gifs = ["hamster", "wolf", "cobra", "lion"];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+// displayGifInfo function re-renders the HTML to display the appropriate content
 function displayGifInfo() {
 
   var gif = $(this).attr("data-name");
-  var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
-// https://api.giphy.com/v1/gifs/search?api_key=91dfe01af7b44084849de62f68879b83&q=dog&limit=25&offset=0&rating=G&lang=en
- queryURL = "gifs/search?api_key=91dfe01af7b44084849de62f68879b83&q=" + gif + "&limit=25&offset=0&rating=G&lang=en";
-
+  var api = "http://api.giphy.com/v1/gifs/search";
+  var apikey = "?api_key=91dfe01af7b44084849de62f68879b83";
+  var query = "&q=" + gif + "&limit=25&offset=0&rating=G&lang=en";
+  //Q:need to diplay the original still and then upon click diplay the gif and vice vera?
+  //A: have a bolean that ill itch from true to fale everytime time it i clicked, on true it i the original till, on fale it i the gif url.
+  var queryURL = api + apikey + query;
+  console.log(queryURL);
+  //queryURL = "http://api.giphy.com/v1/gifs/search?api_key=91dfe01af7b44084849de62f68879b83&q=hamster&limit=10&offset=0&rating=Y&lang=en";
 
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
@@ -18,30 +25,48 @@ function displayGifInfo() {
     method: "GET"
   }).done(function(response) {
     // NEEDS CHECKS HERE!!!!!!!!!!!!!!!
+    // hat kind of check?
+    /*
+    You can also use the StringBuilder class which is mutable.
 
+    StringBuilder sb = new StringBuilder(inputString);
+    It has the method deleteCharAt(), along with many other mutator methods.
+
+    Just delete the characters that you need to delete and then get the result as follows:
+    
+    String resultString = sb.toString();*/
+    //response.data[0].original
+    console.log(response);
     // Creating a div to hold the movie
-    var gifDiv = $("<div class='gif'>");
+    //for loop for all of them
+    for(var i = 0; i<response.data.length; i++){
+      var gifDiv = $("<div class='gif'>");
 
-    // Storing the rating data
-    var rating = response.Rated;
+      // Storing the rating data
+      var rating = response.data[i].rating;
 
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
+      // Creating an element to have the rating displayed
+      var pOne = $("<p>").text("Rating: " + rating);
 
-    // Displaying the rating
-    gifDiv.append(pOne);
+      // Displaying the rating
+      gifDiv.append(pOne);
 
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
+      // Retrieving the URL for the image
+      var imgURL = response.data[i].images.original_still.url;
+      console.log("the first itme: " + imgURL);
+      imgURL = imgURL.substr(5);
+      console.log("the second time: " + imgURL);
+      imgURL = "http" + imgURL;
+      // Creating an element to hold the image
+      var image = $("<img>").attr("src", imgURL);
 
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
-
-    // Appending the image
-    movieDiv.append(image);
+      // Appending the image
+      gifDiv.append(image);
+      $("#animals").prepend(gifDiv);
+    }
 
     // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(movieDiv);
+    
   });
 
 }
@@ -51,7 +76,7 @@ function renderButtons() {
 
   // Deleting the movies prior to adding new movies
   // (this is necessary otherwise you will have repeat buttons)
-  $("#buttons-view").empty();
+  $("#animalSel").empty();
 
   // Looping through the array of movies
   for (var i = 0; i < gifs.length; i++) {
@@ -65,16 +90,18 @@ function renderButtons() {
     a.attr("data-name", gifs[i]);
     // Providing the initial button text
     a.text(gifs[i]);
+    //before appending try to attachfuntion to button that will switch between 
+    //booleans for true it will display origingal_stil and on false it is going to to be the gif
     // Adding the button to the buttons-view div
-    $("#buttons-view").append(a);
+    $("#animalSel").append(a);
   }
 }
 
 // This function handles events where a movie button is clicked
-$("#add-movie").on("click", function(event) {
+$("#addAnimal").on("click", function(event) {
   event.preventDefault();
   // This line grabs the input from the textbox
-  var gif = $("#gif-input").val().trim();
+  var gif = $("#animal-input").val().trim();
 
   // Adding movie from the textbox to our array
   gifs.push(gif);
@@ -83,8 +110,10 @@ $("#add-movie").on("click", function(event) {
   renderButtons();
 });
 
+//https://www.youtube.com/watch?v=fEYx8dQr_cQ
+//call stuff here
 // Adding a click event listener to all elements with a class of "movie"
-$(document).on("click", ".gif", displayGifInfo);
+$(document).on("click", ".gifBtn", displayGifInfo);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
